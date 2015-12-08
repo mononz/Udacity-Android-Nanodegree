d34275c6d1e67412f42d4ddc32a6259bae7ed4dc
Jared Hall <jaredrexhall@gmail.com>
2015-12-08 20:07:57 +1000

    added content description to a couple more views. checking if authors is null before trying to manipulate String, removing some extra resources.

--- P3 - SuperDuo/Alexandria/app/src/main/java/it/jaschke/alexandria/BookDetail.java
+++ P3 - SuperDuo/Alexandria/app/src/main/java/it/jaschke/alexandria/BookDetail.java
@@ -110,9 +110,12 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
         ((TextView) rootView.findViewById(R.id.fullBookDesc)).setText(desc);
 
         String authors = data.getString(data.getColumnIndex(AlexandriaContract.AuthorEntry.AUTHOR));
-        String[] authorsArr = authors.split(",");
-        ((TextView) rootView.findViewById(R.id.authors)).setLines(authorsArr.length);
-        ((TextView) rootView.findViewById(R.id.authors)).setText(authors.replace(",","\n"));
+        if (authors != null && authors.length()>0) { // book might have no authors. check before we try split on a null string
+            String[] authorsArr = authors.split(",");
+            ((TextView) rootView.findViewById(R.id.authors)).setLines(authorsArr.length);
+            ((TextView) rootView.findViewById(R.id.authors)).setText(authors.replace(",", "\n"));
+        }
+
         String imgUrl = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.IMAGE_URL));
         if(Patterns.WEB_URL.matcher(imgUrl).matches()){
             new DownloadImage((ImageView) rootView.findViewById(R.id.fullBookCover)).execute(imgUrl);
@@ -121,8 +124,7 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
 
         String categories = data.getString(data.getColumnIndex(AlexandriaContract.CategoryEntry.CATEGORY));
         ((TextView) rootView.findViewById(R.id.categories)).setText(categories);
-
-        if(rootView.findViewById(R.id.right_container)!=null){
+        if(rootView.findViewById(R.id.right_container) != null){
             rootView.findViewById(R.id.backButton).setVisibility(View.INVISIBLE);
         }
 
