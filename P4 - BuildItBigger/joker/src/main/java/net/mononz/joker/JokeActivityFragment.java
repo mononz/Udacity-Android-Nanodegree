c11974c00c5143dd0c507df58a8f95c497f1d858
Jared Hall <jaredrexhall@gmail.com>
2016-02-21 11:42:00 +1000

    moved async endpoint to fragment, added backend git ignore

--- P4 - BuildItBigger/joker/src/main/java/net/mononz/joker/JokeActivityFragment.java
+++ P4 - BuildItBigger/joker/src/main/java/net/mononz/joker/JokeActivityFragment.java
@@ -1,14 +1,26 @@
 package net.mononz.joker;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+
+import net.mononz.builditbigger.backend.myApi.MyApi;
+
+import java.io.IOException;
+
 public class JokeActivityFragment extends Fragment {
+
+    private TextView jokeTextView;
 
     public JokeActivityFragment() { }
 
@@ -18,12 +30,44 @@ public class JokeActivityFragment extends Fragment {
 
         Intent intent = getActivity().getIntent();
         String joke = intent.getStringExtra(JokeActivity.JOKE_KEY);
-        TextView jokeTextView = (TextView) root.findViewById(R.id.joke_textview);
+        jokeTextView = (TextView) root.findViewById(R.id.joke_textview);
         if (joke != null && joke.length() != 0) {
             jokeTextView.setText(joke);
         }
 
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(getActivity(), "Manfred"));
+
         return root;
+    }
+
+    private class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+
+        private MyApi myApiService = null;
+        private Context context;
+
+        @Override
+        protected String doInBackground(Pair<Context, String>... params) {
+            if(myApiService == null) {
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                        .setRootUrl("https://sonorous-key-745.appspot.com/_ah/api/");
+                myApiService = builder.build();
+            }
+
+            context = params[0].first;
+            String name = params[0].second;
+
+            try {
+                return myApiService.sayHi(name).execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            jokeTextView.setText(result);
+        }
+
     }
 
 }
