e5b9402659fe0f4c5f0c1dbaf88c3e2c35edb10c
Jared Hall <jaredrexhall@gmail.com>
2016-03-26 15:13:39 +1000

    using appcompat. added design lib and updated to build 23 (due to issues, will need to look at permissions on marshmallow). Some minor updates to list activity. added scrim to grid recycler. added base style. wasn't able to get collapsing toolbar working yet

--- P5 - Material/XYZReader/src/main/java/net/mononz/xyzreader/ui/ArticleListActivity.java
+++ P5 - Material/XYZReader/src/main/java/net/mononz/xyzreader/ui/ArticleListActivity.java
@@ -9,7 +9,7 @@ import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
@@ -29,8 +29,7 @@ import net.mononz.xyzreader.data.UpdaterService;
  * touched, lead to a {@link ArticleDetailActivity} representing item details. On tablets, the
  * activity presents a grid of items as cards.
  */
-public class ArticleListActivity extends ActionBarActivity implements
-        LoaderManager.LoaderCallbacks<Cursor> {
+public class ArticleListActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private Toolbar mToolbar;
     private SwipeRefreshLayout mSwipeRefreshLayout;
@@ -42,9 +41,8 @@ public class ArticleListActivity extends ActionBarActivity implements
         setContentView(R.layout.activity_article_list);
 
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
-
-
-        final View toolbarContainerView = findViewById(R.id.toolbar_container);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
 
